aa52742bb96288d5027ed4453d165426bf12afe0
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2022-05-05 16:47:53 +0200

    test: Add GitHub action example

---
+++ tests/DotNet.Testcontainers.ResourceReaper.Tests/GitHub.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNet.Testcontainers.ResourceReaper.Tests
+{
+  using System.Threading.Tasks;
+  using DotNet.Testcontainers.Builders;
+  using DotNet.Testcontainers.Containers;
+  using Xunit;
+
+  public sealed class GitHub : IAsyncLifetime
+  {
+    private readonly IDockerContainer container = new TestcontainersBuilder<TestcontainersContainer>()
+      .WithImage("alpine")
+      .Build();
+
+    [Fact]
+    public void Issue452()
+    {
+      Assert.NotEmpty(this.container.Name);
+    }
+
+    public Task InitializeAsync()
+    {
+      return this.container.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+      return this.container.DisposeAsync().AsTask();
+    }
+  }
+}
